b232671138d53b693de79ef7b7ae3f8ec5fc5d87
Mint Gould <wispymouse@gmail.com>
2024-11-18 22:25:01 -0800

    Start of Game State
    Created the assembly definition of a GameState. Going to work from here towards representing the game state in a better engineered way.

---
+++ Unity/Assets/Scripts/Gamestate/Minimum/Gamestate.cs
@@ -0,0 +1,20 @@
+namespace SpaceDeck.Gamestate.Minimum
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Describes the state of a game.
+    /// 
+    /// This is meant to be entirely hollistic, holding information
+    /// about every character, decision, card, deck, battle, etc.
+    /// 
+    /// This is not necessarily the current game's state. This object
+    /// can be cloned, allowing for theoretical branches of the game's
+    /// state in order to show certain properties and values that might
+    /// exist after something is executed.
+    /// </summary>
+    public class Gamestate
+    {
+    }
+}
