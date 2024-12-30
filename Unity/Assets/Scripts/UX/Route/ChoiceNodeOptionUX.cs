cdaefd7f54576c07d36cd27e3d60456c89099497
Mint Gould <wispymouse@gmail.com>
2024-12-30 15:17:08 -0800

    Finishing Route Folder

--- Unity/Assets/Scripts/UX/Route/ChoiceNodeOptionUX.cs
+++ Unity/Assets/Scripts/UX/Route/ChoiceNodeOptionUX.cs
@@ -1,15 +1,11 @@
 namespace SpaceDeck.UX
 {
-    using SFDDCards;
-    using SFDDCards.UX;
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
 
     public class ChoiceNodeOptionUX : MonoBehaviour
     {
-        [System.Obsolete("Transition to " + nameof(_Representing))]
-        public ChoiceNodeOption Representing;
         public SpaceDeck.GameState.Minimum.ChoiceNodeOption _Representing;
         private ChoiceNodeSelectorUX SelectorUx;
 
@@ -17,16 +13,6 @@ namespace SpaceDeck.UX
         private TMPro.TMP_Text NameLabel;
         [SerializeReference]
         private TMPro.TMP_Text DescriptionLabel;
-
-        [System.Obsolete("Transition to " + nameof(_RepresentOption))]
-        public void RepresentOption(ChoiceNodeSelectorUX selector, ChoiceNodeOption toRepresent)
-        {
-            this.SelectorUx = selector;
-            this.Representing = toRepresent;
-
-            this.NameLabel.text = toRepresent.GetName();
-            this.DescriptionLabel.text = toRepresent.GetDescription();
-        }
 
         public void _RepresentOption(ChoiceNodeSelectorUX selector, SpaceDeck.GameState.Minimum.ChoiceNodeOption toRepresent)
         {
